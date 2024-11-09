2466428ee15b20afe09f6c28333697e8302436ff
kewsor0x <kewsor0x@gmail.com>
2024-11-09 11:34:06 -0300

    first routes

---
+++ internal/users/routes.go
@@ -0,0 +1,25 @@
+package users
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Handler struct {
+}
+
+func NewHandler() *Handler {
+	return &Handler{}
+}
+
+func (h *Handler) RegisterRoutes(router *mux.Router) {
+	router.HandleFunc("/register", h.handleRegister).Methods(http.MethodPost)
+	router.HandleFunc("/login", h.handleLogin).Methods(http.MethodPost)
+}
+
+func (h *Handler) handleLogin(w http.ResponseWriter, r *http.Request) {
+}
+
+func (h *Handler) handleRegister(w http.ResponseWriter, r *http.Request) {
+}
